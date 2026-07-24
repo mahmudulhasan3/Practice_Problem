b15a2e798aa45b913b52e6dae3a55f34230ce9c7
Mahmud <mahmudulhasan8243@gmail.com>
2026-07-24 12:17:49 +0600

    Change into Day-24

--- Day 22-35/Day-24.py
+++ Day 22-35/Day-24.py
@@ -21,26 +21,51 @@
 #         "messange": students_db
 #     }
 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+# book_list = []
+
+
+# class Book(BaseModel):
+#     title: str
+#     author: str
+#     price: float
+#     in_stock: bool
+
+
+# @app.post("/book",status_code=201)
+# def books(book: Book):
+#     book_list.append(book)
+#     return {"message": f"{book.title} added successfully"}
+
+
+# @app.get("/book")
+# def book():
+#     return book_list
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-book_list = []
+class Students(BaseModel):
+    name: str
+    age: int
+    department: str
+    email: str
+    cgpa: float
 
+all_student = []
+@app.post("/studens", status_code=201)
+def student(student: Students):
+    all_student.append(student)
+    return {"message": f"{student} Added Successfully"}
 
-class Book(BaseModel):
-    title: str
-    author: str
-    price: float
-    in_stock: bool
-
-
-@app.post("/book",status_code=201)
-def books(book: Book):
-    book_list.append(book)
-    return {"message": f"{book.title} added successfully"}
-
-
-@app.get("/book")
-def book():
-    return book_list
+@app.get("/students")
+def students():
+    return {
+            "message": "Student Added Successfully",
+            "student":all_student
+            }
