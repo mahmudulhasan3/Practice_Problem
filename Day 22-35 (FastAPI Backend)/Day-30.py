18fca9acf6f043050c6646850964f4f799251b74
Mahmud <mahmudulhasan8243@gmail.com>
2026-07-29 10:29:25 +0600

    mulitpart install

---
+++ Day 22-35 (FastAPI Backend)/Day-30.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI,UploadFile,File
+app = FastAPI()
+
+@app.post("/upload")
+async def upload_pdf(file:UploadFile = File(...)):
+    return{
+        "filename": file.filename,
+        "content_type": file.content_type
+    }
