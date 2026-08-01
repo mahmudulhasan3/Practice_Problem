fe8c5b2fada84a2a818a5a7606505b1c51144fab
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-01 20:59:02 +0600

    API chat

---
+++ OpenAI_Ollama/Api_key_setup.py
@@ -0,0 +1,49 @@
+# import os
+# from dotenv import load_dotenv
+# import google.genai as genai
+
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=api_key)
+
+# response =client.models.generate_content (
+#     model = "gemini-flash-latest",
+#     contents = "Who i am?"
+# )
+
+# print(response.text)
+
+
+# import os
+# from dotenv import load_dotenv
+# import google.genai as genai
+
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=api_key)
+
+# response = client.models.generate_content(
+#     model="gemini-3.5-flash",
+#     contents=input("Enter what you want: ")
+# )
+# print(response.text)
+
+
+import os
+from dotenv import load_dotenv
+import google.genai as genai
+
+try:
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model= "gemini-flash-latest",
+        contents= "Now time"
+    )
+
+    print(response.text)
+
+except Exception as e:
+    print("Something is wrong", e)
