dd7534a5354089db95284b8af58651268e4da91d
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-03 10:42:34 +0600

    logger.json save

--- OpenAI_Ollama/Api_key_setup.py
+++ OpenAI_Ollama/Api_key_setup.py
@@ -172,31 +172,79 @@
 #             time.sleep(0.1)
 # print()
 
-import os, time
-from google import genai
-from dotenv import load_dotenv
+# import os, time
+# from google import genai
+# from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-model_name = os.getenv("GEMINI_MODEL")
-if model_name is None:
-    raise ValueError("GEMINI_MODEL not find in .env file")
-try:
-    while True:
-        prompt = input("\n\nQuestion: ")
-        if prompt == "quit":
-            break
-        stream = client.models.generate_content_stream(
-            model=model_name,
-            contents=prompt,
-            config={"system_instruction": "Answer in 3 line always"},
-        )
-        print("\nAnswer: ")
-        for chunk in stream:
-            if chunk.text:
-                for char in chunk.text:
-                    print(char, end="", flush=True)
-                    time.sleep(0.1)
-except Exception as e:
-    print("Something is wrong", e)
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=api_key)
+# model_name = os.getenv("GEMINI_MODEL")
+# if model_name is None:
+#     raise ValueError("GEMINI_MODEL not find in .env file")
+# try:
+#     while True:
+#         prompt = input("\n\nQuestion: ")
+#         if prompt == "quit":
+#             break
+#         stream = client.models.generate_content_stream(
+#             model=model_name,
+#             contents=prompt,
+#             config={"system_instruction": "Answer in 3 line always"},
+#         )
+#         print("\nAnswer: ")
+#         for chunk in stream:
+#             if chunk.text:
+#                 for char in chunk.text:
+#                     print(char, end="", flush=True)
+#                     time.sleep(0.1)
+# except Exception as e:
+#     print("Something is wrong", e)
+
+
+# import os
+# from google import genai
+# from dotenv import load_dotenv
+# from google.genai import types
+
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=api_key)
+# model_name = os.getenv("GEMINI_MODEL")
+
+# def call_with_logging(system_instruction: str, user_message: str):
+#     stream = client.models.generate_content_stream(
+#         model= model_name,
+#         contents= [{
+#             "role": "user",
+#             "parts": [{"text": user_message}]
+#         }]
+#         config=types.GenerateContentConfig(
+#             "system_instruction": system_instruction
+#     )
+#     usage = stream.use
+
+# from google import genai
+# from dotenv import load_dotenv
+# import os
+# import json
+
+# load_dotenv()
+
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# model_name = os.getenv("GEMINI_MODEL")
+
+# response = client.models.generate_content(
+#     model=model_name, 
+#     contents="Explain FastAPI in simple language."
+# )
+# log = {
+#     "prompt_token": response.usage_metadata.prompt_token_count,
+#     "candidate_token": response.usage_metadata.candidates_token_count,
+#     "total_token": response.usage_metadata.total_token_count
+# }
+
+# with open("logger.jsonl", "a") as file:
+#     file.write(json.dumps(log) + "\n")
+
+
