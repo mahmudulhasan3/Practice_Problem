fa59fb1ab09c5a86c1f2ea4b9302c17a20c86c78
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-16 19:57:41 +0600

    Practice problem

--- OpenAI_Ollama/SQLAlchemy.py
+++ OpenAI_Ollama/SQLAlchemy.py
@@ -20,31 +20,31 @@
 #     session.commit()
 
 
-from sqlalchemy import create_engine, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+# from sqlalchemy import create_engine, String
+# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
-Database_URL = "postgresql://mahmud:CS2203009@localhost:5432/practice_db"
+# Database_URL = "postgresql://mahmud:CS2203009@localhost:5432/practice_db"
 
-engine = create_engine(Database_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
-
-class Base(DeclarativeBase):
-    pass
+# engine = create_engine(Database_URL, echo=True)
+# SessionLocal = sessionmaker(bind=engine)
 
 
-class User(Base):
-    __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
-    email: Mapped[str] = mapped_column(String(100))
+# class Base(DeclarativeBase):
+#     pass
 
 
-Base.metadata.create_all(engine)
-print("✅ Connected & table ready")
+# class User(Base):
+#     __tablename__ = "users"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(100))
+#     email: Mapped[str] = mapped_column(String(100))
 
-with SessionLocal() as session:
-    new_user = User(name="Mahmud", email="mahmud@example.com")
-    session.add(new_user)
-    session.commit()
-print("✅ User created")
+
+# Base.metadata.create_all(engine)
+# print("✅ Connected & table ready")
+
+# with SessionLocal() as session:
+#     new_user = User(name="Mahmud", email="mahmud@example.com")
+#     session.add(new_user)
+#     session.commit()
+# print("✅ User created")
