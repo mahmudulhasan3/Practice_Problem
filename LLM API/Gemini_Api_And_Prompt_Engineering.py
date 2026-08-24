aaac7a593b8ebd3cf74e2731ebec566cf54622bb
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-24 20:32:04 +0600

    Learn system instruction

---
+++ LLM API/Gemini_Api_And_Prompt_Engineering.py
@@ -0,0 +1,24 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+def get_required_env(key):
+    value = os.getenv(key)
+    if not value:
+        raise ValueError(f"Missing value: {key}")
+    return value
+
+api_key= get_required_env("GEMINI_API_KEY")
+client = genai.Client(api_key= api_key)
+
+model_name = get_required_env("GEMINI_MODEL_NAME")
+
+response = client.models.generate_content(
+    model=model_name,
+    config=genai.types.GenerateContentConfig(
+        system_instruction="You are a strict code reviewer, review code short and precise"
+    ),
+    contents="Look at this code:  for i in range(10):   print(i)"
+)
+print(response.text)
