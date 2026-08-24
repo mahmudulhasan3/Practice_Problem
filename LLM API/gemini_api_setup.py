d664aa27594cce5492497d06c6efc3eab36b05da
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-24 20:14:00 +0600

    First LLM Application + Model Selection

--- LLM API/gemini_api_setup.py
+++ LLM API/gemini_api_setup.py
@@ -1,15 +1,26 @@
-from google import genai
-from dotenv import load_dotenv
-import os
+# from google import genai
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-if api_key is None:
-    raise ValueError("API key is not found")
-client = genai.Client(api_key= api_key)
+# load_dotenv()
+# def get_required_env(key):
+#     value = os.getenv(key)
+#     if not value:
+#         raise ValueError(f"Missing value: {key}")
+#     return value
 
-response = client.models.generate_content(
-    model="gemini-3.1-flash-lite", 
-    contents="What is ai"
-)
-print(response.text)
+# api_key= get_required_env("GEMINI_API_KEY")
+# client = genai.Client(api_key= api_key)
+
+# model_name = get_required_env("GEMINI_MODEL_NAME")
+
+# response1 = client.models.generate_content(
+#     model=model_name, 
+#     contents="What is ai"
+# )
+# response2 = client.models.generate_content(
+#     model = model_name,
+#     contents= "Who is Andrew Ng?"
+# )
+# print(response1.text)
+# print(response2.text)
