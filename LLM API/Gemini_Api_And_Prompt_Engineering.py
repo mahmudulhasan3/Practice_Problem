c0a3612d4e395bffa71df7f065a31faf620ed668
Mahmud <mahmudulhasan8243@gmail.com>
2026-08-26 11:17:50 +0600

    Perse json and validate

--- LLM API/Gemini_Api_And_Prompt_Engineering.py
+++ LLM API/Gemini_Api_And_Prompt_Engineering.py
@@ -168,33 +168,73 @@
 
 # print(response.text.strip())
 
+# from google import genai
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+
+# def get_required_key(key):
+#     value = os.getenv(key)
+#     if not value:
+#         raise ValueError(f"{key} is not found")
+#     return value
+
+
+# api_key = get_required_key("GEMINI_API_KEY")
+# client = genai.Client(api_key=api_key)
+
+# model_name = get_required_key("GEMINI_MODEL_NAME")
+
+# prompt = """
+# What is 8473 multiplied by 6291? 
+# Let's think step by step, breaking down the multiplication, 
+# then give the final answer.
+# """
+# response = client.models.generate_content(
+#     model=model_name,
+#     contents=prompt,
+# )
+
+# print(response.text)
+
+
 from google import genai
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field, field_validator
 import os
 
 load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key= api_key)
+
+model_name = os.getenv("GEMINI_MODEL_NAME")
+if not model_name:
+    raise ValueError("Model name not found")
+class Base(BaseModel):
+    name: str
+    email: str | None
+    skills: list[str]
+    experience: int = Field(ge=0)
+
+    @field_validator("email")
+    @classmethod
+    def check_email(cls, value):
+        if value is not None and "@" not in value:
+            raise ValueError("Invalid email formet")
+        return value
 
 
-def get_required_key(key):
-    value = os.getenv(key)
-    if not value:
-        raise ValueError(f"{key} is not found")
-    return value
-
-
-api_key = get_required_key("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
-model_name = get_required_key("GEMINI_MODEL_NAME")
-
-prompt = """
-What is 8473 multiplied by 6291? 
-Let's think step by step, breaking down the multiplication, 
-then give the final answer.
-"""
 response = client.models.generate_content(
-    model=model_name,
-    contents=prompt,
+    model= model_name,
+    contents= """আমার নাম Mahmud, email: mahmudexample.com। 
+আমি Python, FastAPI এবং SQL এ কাজ করি। 
+আমার ২ বছরের experience আছে।""",
+    config= {
+        "response_mime_type": "application/json",
+        "response_schema": Base
+    }
 )
-
-print(response.text)
+person = Base.model_validate_json(response.text)
+print(person)
